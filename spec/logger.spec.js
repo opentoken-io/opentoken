cfecd9ce4a2293c3ee7d5d2a3664c75c853156b5
Tyler Akins <fidian@rumkin.com>
2016-04-06 09:44:52 -0500

    Using ES6 style functions and moving "use strict"

--- spec/logger.spec.js
+++ spec/logger.spec.js
@@ -1,53 +1,54 @@
-describe("logger", function () {
-    "use strict";
+"use strict";
+
+describe("logger", () => {
     var log, logger;
 
-    beforeEach(function () {
+    beforeEach(() => {
         logger = require("../lib/logger");
 
         spyOn(console, "log");
         spyOn(console, "error");
     });
 
-    describe("constructor", function () {
-        beforeEach(function () {
+    describe("constructor", () => {
+        beforeEach(() => {
             log = new logger({});
         });
 
-        it("prints to log from info", function () {
+        it("prints to log from info", () => {
             log.info("something");
             expect(console.log).toHaveBeenCalledWith("something");
         });
 
-        it("prints to error from error", function () {
+        it("prints to error from error", () => {
             log.error("something");
             expect(console.error).toHaveBeenCalledWith("ERROR: something");
         });
 
-        it("prints to error from warn", function () {
+        it("prints to error from warn", () => {
             log.warn("something");
             expect(console.error).toHaveBeenCalledWith("WARN: something");
         });
 
-        it("prints to log from console", function () {
+        it("prints to log from console", () => {
             log.console("something");
             expect(console.log).toHaveBeenCalledWith("something");
         });
 
-        it("does not print to error from debug", function () {
+        it("does not print to error from debug", () => {
             log.debug("something");
             expect(console.error).not.toHaveBeenCalledWith("something");
         });
     });
 
-    describe("constructor with debug set", function () {
-        beforeEach(function () {
+    describe("constructor with debug set", () => {
+        beforeEach(() => {
             log = new logger({
                 debug: true
             });
         });
 
-        it("prints to error from debug", function () {
+        it("prints to error from debug", () => {
             log.debug("something");
             expect(console.error).toHaveBeenCalledWith("DEBUG: something");
         });
