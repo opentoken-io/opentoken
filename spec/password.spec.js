6e7293973d415e9315564e7663df2d4370c94aad
Christopher Tetreault <tetreault.chris@gmail.com>
2016-04-26 13:42:28 +0000

    Using actual cryto to tests

--- spec/password.spec.js
+++ spec/password.spec.js
@@ -1,32 +1,23 @@
 "use strict";
 
 describe("password", ()  => {
-    var password, cryptoFake;
+    var password, crypto;
 
     beforeEach(() => {
         var config;
 
-        cryptoFake = jasmine.createSpyObj("cryptoFake", [
-            "createHash",
-            "digest",
-            "update"
-        ]);
-        cryptoFake.createHash = jasmine.createSpy("crypto.createHash").andReturn(cryptoFake);
-        cryptoFake.digest = jasmine.createSpy("crypto.digest").andReturn("hashedcontent");
-        cryptoFake.update = jasmine.createSpy("crypto.update").andReturn(cryptoFake);
+        crypto = require("crypto");
         config = {
             password: {
                 hashAlgo: "sha256"
             }
         };
-        password = require("../lib/password")(config, cryptoFake);
+        password = require("../lib/password")(config, crypto);
     });
     it("hashes a passed in value", () => {
-        expect(password.hashContent("contenttohash")).toBe("hashedcontent");
-        expect(cryptoFake.update).toHaveBeenCalledWith("contenttohash");
+        expect(password.hashContent("rRTcBER_EiFUsRa34Hj5Zpok")).not.toBe("rRTcBER_EiFUsRa34Hj5Zpok");
     });
     it("returns false as there's nothing to hash", () => {
         expect(password.hashContent("")).toBe(false);
-        expect(cryptoFake.update).not.toHaveBeenCalled();
     });
 });
