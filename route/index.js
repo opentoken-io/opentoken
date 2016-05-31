be034cf7d9987bbc13cb0645becded5fbb019b19
Tyler Akins <fidian@rumkin.com>
2016-05-31 14:42:56 -0500

    Allow serving schemas via API
    This means I have to include the .json extension when validating against
    a schema.

--- route/index.js
+++ route/index.js
@@ -1,11 +1,21 @@
 "use strict";
 
-module.exports = () => {
-    return {
-        get(req, res, next) {
-            res.setHeader("Content-Type", "text/plain");
-            res.send(`API running ${new Date()}\n`);
-            next();
+module.exports = (server, pathUrl, options) => {
+    return options.container.call((config, path, restifyPlugins) => {
+        if (pathUrl.charAt(path.length - 1) === "/") {
+            server.get(/\/schema\/.*/, restifyPlugins.serveStatic({
+                charSet: "utf-8",
+                directory: path.resolve(config.baseDir),
+                match: /\.json$/
+            }));
         }
-    };
+
+        return {
+            get(req, res, next) {
+                res.setHeader("Content-Type", "text/plain");
+                res.send(`API running ${new Date()}\n`);
+                next();
+            }
+        };
+    });
 };
