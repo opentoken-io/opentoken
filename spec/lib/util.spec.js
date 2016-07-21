1346a0448b1b360964f2576e1575ea2fe68542f1
Tyler Akins <fidian@rumkin.com>
2016-07-21 08:45:10 -0500

    Additional clone tests

--- spec/lib/util.spec.js
+++ spec/lib/util.spec.js
@@ -19,5 +19,15 @@ describe("util", () => {
             expect(input).not.toBe(output);
             expect(input.a).not.toBe(output.a);
         });
+        it("clones null", () => {
+            expect(util.clone(null)).toBe(null);
+        });
+        it("clones a string", () => {
+            expect(util.clone("a string")).toBe("a string");
+        });
+        it("converts string objects into strings", () => {
+            /* eslint no-new-wrappers:"off" */
+            expect(util.clone(new String("xx"))).toBe("xx");
+        });
     });
 });
